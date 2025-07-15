6a63e9ef875df8ab741080b30473d9694b0a7a54
mohamedidrisssalhi <mohamedidrisssalhi@gmail.com>
2025-07-15 18:56:19 +0100

    tc

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use('/api/tasks', require('./routes/tasks'));
 app.use(express.static(path.join(__dirname, 'client', 'build')));
 
 // Handle React routing, return all requests to React app
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 
